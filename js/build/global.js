82e94a5f22944fbc8fd531e8a6d5b30edbc38ecb
Andy Beckmann <abec19@gmail.com>
2021-04-09 22:21:18 -0400

    Add cookie setter for close button

--- js/build/global.js
+++ js/build/global.js
@@ -1,5 +1,21 @@
-function modalSignUp() {
-	document.getElementById('modal--close').addEventListener('click', function() {
-		document.getElementById('modal').style.display = 'none';
+function setCookie(key, value, expiration) {
+	document.cookie = key + '=' + value + '; expires=' + expiration + '; path=/';
+}
+
+function modal() {
+
+	let modalWindow = document.getElementById('modal'),
+		closeButton = document.getElementById('modal--close'),
+		expirationDate = new Date();
+
+	if (document.cookie.split('; ').find(row => row.startsWith('popup_close'))) {
+		modalWindow.style.display = 'none';
+	}
+		
+	expirationDate.setHours(expirationDate.getUTCHours() + 12);
+
+	closeButton.addEventListener('click', function() {
+		modalWindow.style.display = 'none';
+		setCookie('popup_closed', 'true', expirationDate);
 	});
 }
